e27aa8e275f08f195db6294bc11731ed27bdb948
SeokjinKim00 <tjrwls1108011@gmail.com>
2023-01-02 20:55:22 +0900

    Feat : Get Title, Author in Google Scholar

--- Source/scholar.py
+++ Source/scholar.py
@@ -1,6 +1,25 @@
+from selenium.webdriver.common.by import By
+
 def getScholarURL(question, page):
     urlBefore = 'https://scholar.google.co.kr/scholar?start='
     searchQuery = '&q='+question
     urlAfter = '&hl=ko&as_sdt=0,5'
     url = urlBefore + str(page) + searchQuery + urlAfter
     return url
+
+def getScholarTitle(driver):
+    titleList=list()
+    title = driver.find_elements(By.CLASS_NAME, 'gs_rt')
+    for i in title:
+        if i is not None:
+            titleList.append(str(i.text))
+    
+    return titleList
+
+def getAuthorScholar(driver):
+    authorList=list()
+    author=driver.find_elements(By.CLASS_NAME, 'gs_a')
+    for i in author:
+        if i is not None:
+            authorList.append(str(i.text))
+    return authorList
