fda1d133871252970f7d7960a276b8bf1b23368a
SeokjinKim00 <tjrwls1108011@gmail.com>
2023-01-14 00:57:13 +0900

    Feat : RISS Check Data Num

--- Source/rissScrap.py
+++ Source/rissScrap.py
@@ -1,5 +1,8 @@
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup as bs
+
 
 def textInput():
     print("Search Text")
@@ -18,3 +21,21 @@ def rissSearchPage(driver, query):
     driver.find_element(By.ID,'query').click( )
     element.send_keys(query)
     element.send_keys(Keys.ENTER)
+
+def checkDataNum(driver):
+    html = driver.page_source
+    soup = bs(html, 'html.parser')
+
+    referNumDict = dict()
+    num1 = soup.find_all('div', 'title')
+
+    strDel1 = '[<span class="moreCnt">'
+    strDel2 = ','
+    strDel3 = '</span>]'
+
+    for n, t in zip(range(1, 8), num1):
+        referNum = str(t.select('h3 > span'))
+        referNum = referNum.replace(strDel1, '').replace(strDel2, '').replace(strDel3, '')
+        referNumDict[n] = int(referNum)
+        
+    return referNumDict
